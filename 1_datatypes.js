a6a4f62bcc1e58526fdd14159115a53d99e521b5
samik ghorai <samikghorai155@gmail.com>
2025-12-20 20:48:43 +0530

    Add JavaScript datatypes file

---
+++ 1_datatypes.js
@@ -0,0 +1,56 @@
+// primitive data types
+
+// number:  
+// var myfavnum = "134";
+// console.log(myfavnum);
+
+// srting represent a single or double quotes 
+// var myname= "samik"
+// console.log(myname)
+
+// boolean is defined true or false
+//  var israining=false
+//  var iamgood=true
+//  console.log(israining)
+
+// undefined
+// var samik
+// console.log(samik)
+
+// null
+// var badname=null
+// console.log(badname)
+
+// bigint
+// const bignumber=12589632327741542552455210650n
+
+// symbol
+
+
+
+// what is the purpose of typeof operator in js?
+
+// var myname =5
+// console.log(myname)
+// console.log(typeof myname)
+
+// convert a string to a number?
+
+// var mynum="23"
+// console.log(typeof +mynum)
+// console.log(typeof Number(mynum))
+
+// convert a number to a string?
+
+// var mynum=7
+// console.log(typeof String(mynum))
+// console.log(typeof (mynum+""))
+
+// check true or false
+
+var myname=""
+if(myname){
+    console.log("true")
+}else{
+    console.log("false")
+}
