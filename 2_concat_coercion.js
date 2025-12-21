b0701363075ab7ca0ad9219dfe10cb1c26894c84
samik ghorai <samikghorai155@gmail.com>
2025-12-21 20:18:02 +0530

    concat_coercion.js

---
+++ 2_concat_coercion.js
@@ -0,0 +1,19 @@
+// concatenation in js
+
+// const str ="hello "+"world"
+// console.log(str)
+
+// type coercion
+
+let sum="9" + 8
+console.log(sum)
+
+console.log(9-"8")
+console.log("java"+"script")
+console.log(""+"")
+console.log(""+0)
+console.log("samik"-"ghorai")
+console.log(true+true)
+console.log(true+false)
+console.log(false+true)
+console.log(false-true)
