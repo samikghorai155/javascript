ebad4a05c7d8a0457027324ebbfb7161479654c9
samik ghorai <samikghorai155@gmail.com>
2025-12-23 15:38:01 +0530

    expression js

---
+++ 3_expressions.js
@@ -0,0 +1,97 @@
+//1. Assignment operators
+
+// var myfavnum = 37    assign value 15
+// var myinsta ="samik2006"    = is the assign value 
+
+// 2. Arithmetic operator
+
+// a. addition (+)
+//    var y= 20
+//    var x=10
+//    var sum=x+y
+//    console.log(sum)
+ 
+// b. subtraction (-)
+    //  var a=28
+    //  var b=16
+    //  var difference=a-b 
+    //  console.log(difference)
+
+//   c .multipliction (*)
+    // var a=20
+    // var b=20
+    // var c=5
+    // var product=a*b*c
+    // console.log(product)  
+
+//   d . division
+    // var p=30
+    // var q =2
+    // var quotient=p/q
+    // console.log(quotient)  
+
+//   e.modulus
+// var a=26
+// var b=3
+// var remainder=a%b
+// console.log(remainder)  
+
+// challange time 
+// var result="hello"/2
+// console.log(result)
+
+// var result=0.7+0.1
+// console.log(result toFixed(2))
+
+// 3. srting operators
+
+// var str1 ="hello"
+// var str2="world"
+// var str3=str1+str2
+// console.log(str3)
+
+// 4. Comparison operators
+
+// a . equal
+// console.log(3==5)
+
+// b . strict equal
+// console.log(5==="5")
+
+// c . not equal
+// console.log(5!=5)
+
+// d .greater than 
+// console.log(5>3)
+
+// e .less than 
+// console.log(10<5)
+
+// f .greater than or equal to 
+// console.log(10>=10)
+
+// 5. Logical operators
+
+// a .Logical (And &&)
+// var x=5
+// var y=15
+// console.log(x>0 && y<0)
+
+// b .Logical ( OR ||)
+// var x=15
+// var y=0
+// console.log(x<0 || y>0)
+
+// c .Logical NOT (!)
+// var isopen=true
+// console.log(!isopen)
+
+// 6. Condition (Tenary) operator
+// syntex : condition? expression if true : expression if false;
+//  var age=19
+//  var result=age>=18 ? "yes" : "no"
+//  console.log(result)
+
+ var score=88
+ var result=score>=60 ? "pass" : "fail"
+ console.log(result)
