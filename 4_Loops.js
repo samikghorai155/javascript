5adb60617d0135dfdda9f35e5ca549fd525bf3c3
samik ghorai <samikghorai155@gmail.com>
2025-12-24 16:39:11 +0530

    loops

---
+++ 4_Loops.js
@@ -0,0 +1,143 @@
+// var temperature=25
+// if (temperature >30){
+//     console.log("I go to groud today")
+// } else{
+//     console.log("I watch TV in home")
+// }
+
+
+// var userage=20
+// var iscitizen=true
+// var isregistered=false 
+
+// if(userage>=18){
+// if(iscitizen){
+// if(isregistered){
+// console.log("you are alligible to vote")
+// }else{
+//     console.log("you are not aligible due to not registered")
+// }
+// }else{
+//     console.log("you are not aligible due to not citizen")
+// }
+// }else{
+//     console.log("you are not aligible to vote (younger)")
+// }
+
+// ODD & EVEN 
+
+// var num=8
+
+// if(num % 2===0){
+//     console.log("num is even")
+// }else{
+//     console.log("num is odd")
+// }
+
+// positive , negative & zero
+
+// var num=-57
+
+// if(num===0){
+//     console.log("num is zero")
+// }else if (num>0){
+//     console.log("num is positive")
+// }else{
+//     console.log("num is negative")
+// }
+
+// PRIME OR NOT PRIME 
+
+// var num =12
+// var isprime=true
+
+// for(i=2 ; i<num ; i++)
+//     if(num% i===0){
+//        isprime=false
+//        break
+//     }
+//     if(isprime){
+//         console.log("num is prrime")
+//     }else{
+//         console.log("number is not prime")
+//     }
+
+// Switch statement 
+
+// var day="friday"
+
+// switch(day){
+//     case "monday":console.log("today i go to school")
+//     break
+
+//     case "friday":console.log("lets attend the party today")
+//     break
+
+//     case "sunday":console.log("lets go to movie")
+//     break
+
+//     default :console.log("no condition")
+// }
+
+// var areaofshapes = "rectengle"
+// var a=5
+// var b=10
+// var result
+// switch(areaofshapes){
+//     case "square":
+//         result=a*a
+//         console.log(result)
+//         break
+
+//         case "rectengle":
+//             result=a*b 
+//             console.log(result)
+//             break
+
+//             case "circle":
+//                 var r=2
+//                 result=3.142 *(r*r)
+//                 console.log(result)
+//                 break
+
+//                 default:console.log("sorry no shapes match")
+// }
+
+// while loop 
+
+// var num=1
+// while(num<=10){
+//     console.log(num)
+//     num++
+// }
+
+// var num=1
+// while(num<=10){
+//     console.log("5 * " + num + " = " +(5*num))
+//     num++
+// }
+
+// do-while loop 
+
+// var num=1
+// do{
+//     console.log(num)
+//     num++
+// } while(num<=10)
+
+
+// for loop
+
+// for (let num=1; num<=10; num++){
+//     console.log(num)
+// }
+
+// var num=1
+// while(num<=10){
+//     console.log("5 * " + num + " = " +(5*num))
+//     num++
+// }
+
+for(var num=1; num<=10; num++){
+     console.log("5 * " + num + " = " +(5*num))
+}
